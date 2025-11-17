8caac564dd51cea9bef4c757c7979d3a5d09e4a9
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-16 20:07:28 -0800

    add spinner back

---
+++ frontend/src/components/LoadingScreen.tsx
@@ -0,0 +1,11 @@
+import "./LoadingScreen.scss";
+
+function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default LoadingScreen;
