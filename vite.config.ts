3e7118c23d5d6209ce5376af92ebd258be8ab7ba
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-29 22:00:09 -0800

    seo optimization for another day ig

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import metaMapPlugin from "vite-plugin-react-meta-map";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    metaMapPlugin({
-      pageMetaMapFilePath: "./src/pageMetaMap.ts",
-      pageTemplateFilePath: "./src/PageTemplate.tsx",
-    }),
-  ],
+  plugins: [react()],
   publicDir: "public",
   build: {
     minify: true,
